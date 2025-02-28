0ff42176a5be3cf2d1f087f5f802ef584bb92092
desire627 <desireasiimwe24@gmail.com>
2025-02-28 16:19:31 +0300

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import InstructorDashboard from "./pages/InstructorDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
 import Login from "./pages/Login";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/instructor" element={<InstructorDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
-}
+};
 
 export default App;
